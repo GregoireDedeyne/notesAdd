258ad1950cbf6663c573f8aeceea0579a0a39217
GregoireDedeyne <greg.dedeyne@gmail.com>
2024-03-05 20:13:23 +0100

    change Type

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,12 +6,14 @@ import { Footer } from '../Footer/Footer';
 
 interface Note {
   content: string;
-  date: string; // Champ de date ajouté
+  date: string; 
 }
 
 function App() {
+  // state pour stocker les notes
   const [notes, setNotes] = useState<Note[]>([]);
 
+  // use effect pour la mise en place du localStorage au démarrage de l'app
   useEffect(() => {
     const savedNotes = localStorage.getItem('notes');
     if (savedNotes) {
@@ -20,23 +22,28 @@ function App() {
     console.log(savedNotes);
   }, []);
 
+  // function de génération de date
   function generateDate() {
     return new Date().toLocaleDateString();
   }
-  // Sauvegarde des notes dans le localStorage chaque fois que les notes changent
+
+  // Sauvegarde des notes dans le localStorage
   useEffect(() => {
     localStorage.setItem('notes', JSON.stringify(notes));
     console.log(notes);
   }, [notes]);
 
+  // function d'ajout des nouvelles notes avec la date généré 
   const addNote = (content: string) => {
     const newNote: Note = {
       content: content,
-      date: generateDate(), // Stockez la date actuelle avec la note
+      date: generateDate(), 
     };
+    // on appel setNotes pour mettre à jour les notes dans le state
     setNotes([...notes, newNote]);
   };
 
+  // Function d'édition de nos notes
   const handleEdit = (originalContent: Note, editedContent: Note) => {
     const updatedNotes = notes.map((note) => {
       // Comparez les propriétés de chaque objet Note
@@ -53,7 +60,7 @@ function App() {
   };
   const handleDelete = (content: string) => {
     const updatedNotes = notes.filter((note) => note.content !== content); // Supprime le post avec le contenu correspondant
-    setNotes(updatedNotes); // Met à jour la liste de posts
+    setNotes(updatedNotes); // Met à jour la liste de notes
   };
 
   return (
